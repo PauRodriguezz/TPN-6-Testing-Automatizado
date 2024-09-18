9d95d3fcac0ec982841b1461bc338a70acc8d7da
FranManuel55 <fnmanuel55@gmail.com>
2024-09-18 20:11:05 -0300

    Email Test

--- cypress/e2e/add_contact.cy.js
+++ cypress/e2e/add_contact.cy.js
@@ -17,3 +17,45 @@ describe('First Name Input Tests', () => {
         cy.get(firstNameInput).should('have.value', firstName);
     });
 });
+
+//Validacion del Correo Electronico 
+describe('Validación del campo de correo electrónico', () => {
+    
+    const emailInput = () => cy.get('#email');
+
+    const fillEmail = (email) => {
+        emailInput().type(email);
+    };
+
+    const getEmailValue = () => {
+        return emailInput().invoke('val');
+    };
+
+    beforeEach(() => {
+        cy.visit('https://thinking-tester-contact-list.herokuapp.com/addContact');
+    });
+
+    it('Debería aceptar un formato de correo electrónico válido', () => {
+        const validEmail = 'test@example.com';
+        fillEmail(validEmail);
+        getEmailValue().should('eq', validEmail);
+    });
+
+    it('Debería rechazar un formato de correo electrónico inválido - falta el "@"', () => {
+        const invalidEmail = 'testexample.com';
+        fillEmail(invalidEmail);
+        getEmailValue().should('eq', invalidEmail);
+    });
+
+    it('Debería rechazar un formato de correo electrónico inválido - falta el dominio', () => {
+        const invalidEmail = 'test@.com';
+        fillEmail(invalidEmail);
+        getEmailValue().should('eq', invalidEmail);
+    });
+
+    it('Debería rechazar un formato de correo electrónico inválido - falta la parte local', () => {
+        const invalidEmail = '@example.com';
+        fillEmail(invalidEmail);
+        getEmailValue().should('eq', invalidEmail);
+    });
+});
