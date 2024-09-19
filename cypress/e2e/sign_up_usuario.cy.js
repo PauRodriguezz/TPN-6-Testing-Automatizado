22cc20c5b1259d759ad7aa5b4dbb88d192aa6ad4
RodriSLK <sanzrodrigo36@gmail.com>
2024-09-19 12:08:18 -0300

    Test de registro

--- cypress/e2e/sign_up_usuario.cy.js
+++ cypress/e2e/sign_up_usuario.cy.js
@@ -12,7 +12,7 @@ describe('Button Tests', () => {
       cy.url().should('include', '/addUser');
     });
 
-    //Segundo Test
+    //Test prueba de boton
     it('Confirme que el texto del botón sea claramente legible e indique correctamente "Sign up".', () => {
       cy.get('button')  // Asegúrate de usar el selector correcto para el botón "Sign up"
         .should('be.visible')
@@ -20,6 +20,26 @@ describe('Button Tests', () => {
     });
 
   });
+
+// Tests para el registro de usuario
+describe('User Registration', () => {
+  beforeEach(() => {
+    // Visita la URL antes de cada test
+    cy.visit('https://thinking-tester-contact-list.herokuapp.com/addUser');
+  });
+
+  it('Verifique que el usuario pueda completar correctamente los campos de nombre, apellido, correo electrónico y contraseña y enviar el formulario.', () => {
+    // Completar el formulario con los datos
+    cy.get('#firstName').type('Leonel10');
+    cy.get('#lastName').type('Messi');
+    cy.get('#email').type('leomessi1000@gmail.com');
+    cy.get('#password').type('campeonDelMundo');
+
+    // Enviar el formulario
+    cy.get('#submit').click();
+
+  });
+});
 
 
   
